c2320c96217f16ce60137f292def08b2e23c4c63
Nithanth Sujith <snithanth@gmail.com>
2026-05-17 00:39:18 +0530

    Chat list added

--- src/components/ChatCard.tsx
+++ src/components/ChatCard.tsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import clsx from "clsx";
 
-const ChatCard = () => {
-    return (
-        <div className='flex p-3 items-center text-[#E5E2E1] gap-3 cursor-pointer rounded-xl hover:bg-[#313030] transition-all duration-200'>
-            <div className='size-12 rounded-full bg-black relative'>
-                <img src="./Avatar_1.png" alt="img" className='object-cover' />
-                <div className='size-3 rounded-full p-1 bg-black bottom-1 right-1 absolute'>
-                    <p className='size-2 rounded-full bg-[#00DBE7] '></p>
-                </div>
-            </div>
-            <div className='flex flex-col gap-1 flex-1'>
-                <div className='flex justify-between items-center '>
-                    <div className='text-md font-semibold'>Elena Vance</div>
-                    <div className='text-xs text-[#737373]'>12:45 PM</div>
-                </div>
-                <div className='text-[#A3A3A3] text-sm w-3/4'>The neural interface is stable now...</div>
-            </div>
-        </div>
-    )
+interface ChatCardProps {
+  username: string;
+  timestamp: string;
+  lastMessage: string;
+  imageURL: string;
+  isActive: boolean;
+  isOnline: boolean;
 }
 
-export default ChatCard
+const ChatCard = ({
+  username,
+  timestamp,
+  lastMessage,
+  imageURL,
+  isActive,
+  isOnline,
+}: ChatCardProps) => {
+  return (
+    <div
+      className={clsx(
+        "flex px-5 py-4 items-center text-[#E5E2E1] gap-4 cursor-pointer rounded-xl  transition-all duration-200",
+        isActive && "bg-[#313030] hover:bg-[#313030]",
+        !isActive && "hover:bg-[#313030]/70",
+      )}
+    >
+      <div className="size-12 rounded-full bg-black relative">
+        <img src={imageURL} alt="img" className="object-cover" />
+        {isOnline && (
+          <div className="size-4 rounded-full p-1 bg-black bottom-1 -right-1 absolute flex justify-center items-center">
+            <p className="w-full h-full rounded-full bg-[#00DBE7] "></p>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col gap-1 flex-1">
+        <div className="flex justify-between items-center ">
+          <div className="text-md font-semibold">{username}</div>
+          <div className="text-xs text-[#737373]">{timestamp}</div>
+        </div>
+        <div className="text-[#A3A3A3] text-sm w-3/4">{lastMessage}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatCard;
--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -2,14 +2,46 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import ChatCard from './ChatCard'
 
+
+const CHAT_LIST = [
+    {
+        username : "Elena Vance",
+        timeStamp : "12:45 PM",
+        lastMessage : "The neural interface is stable now...",
+        imageURl : "./Avatar_1.png",
+        isActive : true,
+        isOnline : true,
+    },
+    {
+        username : "Marcus Kaine",
+        timeStamp : "Yesterday",
+        lastMessage : "Did you see the latest uplink speed?",
+        imageURl : "./Avatar_2.png",
+        isActive : false,
+        isOnline : false,
+    },
+    {
+        username : "Sasha Grey",
+        timeStamp : "Monday",
+        lastMessage : "Meeting at 09:00 encryption secure.",
+        imageURl : "./Avatar_3.png",
+        isActive : false,
+        isOnline : false,
+    },
+] 
+
 const ChatList = () => {
     return (
         <div className='w-sm h-screen bg-[#0A0A0A] border-r border-r-[#1C1B1B]/70 flex flex-col'>
             <div className='p-5'>
                 <SearchBar />
             </div>
-            <div className='p-5 pt-0 '>
-                <ChatCard/>
+            <div className='p-5 pt-0 flex flex-col gap-1'>
+                {
+                    CHAT_LIST.map((chat, index)=>(
+                        <ChatCard username={chat.username} timestamp={chat.timeStamp} lastMessage={chat.lastMessage} imageURL={chat.imageURl} isActive={chat.isActive} isOnline={chat.isOnline} />
+                    ))
+                }
             </div>
         </div>
     )
