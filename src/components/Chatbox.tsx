e205441dc062bc16e4695e034e093634810abca6
Nithanth Sujith <snithanth@gmail.com>
2026-06-16 00:29:11 +0530

    Minor Updates

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -28,7 +28,7 @@ const Chatbox = () => {
   return (
     <div className="h-screen flex-1 bg-[#0E0E0E] border-r border-r-[#1C1B1B]/70 flex flex-col">
       <div className="w-full h-[calc(100%-6.25rem)] relative pt-5">
-        <div className="text-[#737373] border left-1/2 w-fit m-auto px-4 py-1 rounded-full text-sm flex justify-center items-center pb-1.5 border-[#313030]/70 bg-[#171717]/80">
+        <div className="text-[#737373] border  left-1/2 w-fit m-auto px-4 py-1 rounded-full text-sm flex justify-center items-center pb-1.5 border-[#313030]/70 bg-[#171717]/80">
           Today, June 2nd
         </div>
         <div className="w-full h-full flex-col flex p-5 gap-3 overflow-y-auto chatScrollbar">
@@ -42,7 +42,7 @@ const Chatbox = () => {
       </div>
       <div className="w-full h-25 p-5 flex items-center  gap-3 bg-[#0E0E0E]/10 backdrop-blur-xl">
         <div className="flex-1 rounded-xl border border-[#313030]/70 bg-[#171717]/80 h-12"></div>
-        <div className="size-10 rounded-lg bg-[#B76DFF] flex justify-center items-center cursor-pointer hover:bg-[#c082ff] duration-200 transition-all"><MdSend size={20} color="#2C0051" /></div>
+        <div className="size-10 rounded-lg bg-[#DDB7FF] flex justify-center items-center cursor-pointer hover:bg-[#c082ff] duration-200 transition-all"><MdSend size={20} color="#490080" /></div>
       </div>
     </div>
   );
